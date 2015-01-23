1752100997baa55941cf65154c90b0f8c788ccc5
Dani Huertas <huertas.dani@gmail.com>
2015-01-23 09:20:20 +0000

    Wrapped INP_GPIO macro in OUT_GPIO and SET_GPIO_ALT

--- PJ_RPI.h
+++ PJ_RPI.h
@@ -37,8 +37,14 @@ extern struct bcm2835_peripheral bsc0;	// so use extern!!
 
 // GPIO setup macros. Always use INP_GPIO(x) before using OUT_GPIO(x) or SET_GPIO_ALT(x,y)
 #define INP_GPIO(g) 	*(gpio.addr + ((g)/10)) &= ~(7<<(((g)%10)*3))
-#define OUT_GPIO(g) 	*(gpio.addr + ((g)/10)) |=  (1<<(((g)%10)*3))
-#define SET_GPIO_ALT(g,a) *(gpio.addr + (((g)/10))) |= (((a)<=3?(a) + 4:(a)==4?3:2)<<(((g)%10)*3))
+#define OUT_GPIO(g)   do { \
+  INP_GPIO(g); \
+  *(gpio.addr + ((g)/10)) |=  (1<<(((g)%10)*3)); \
+} while(0)
+#define SET_GPIO_ALT(g,a) do { \
+  INP_GPIO(g); \
+  *(gpio.addr + (((g)/10))) |= (((a)<=3?(a) + 4:(a)==4?3:2)<<(((g)%10)*3)); \
+} while(0)
 
 #define GPIO_SET 	*(gpio.addr + 7)  // sets   bits which are 1 ignores bits which are 0
 #define GPIO_CLR 	*(gpio.addr + 10) // clears bits which are 1 ignores bits which are 0
